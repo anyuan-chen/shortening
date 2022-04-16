a2e19bdb27930d842898e11ba0e90848e4de4497
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-04-15 20:37:43 -0400

    added routing

--- server/go.mod
+++ server/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/itchyny/base58-go v0.2.0 // indirect
 	github.com/jessevdk/go-flags v1.5.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
--- server/main.go
+++ server/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/anyuan-chen/urlshortener/server/util"
+import (
+	"github.com/anyuan-chen/urlshortener/server/handler"
+	"github.com/anyuan-chen/urlshortener/server/store"
+	"github.com/anyuan-chen/urlshortener/server/util"
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	util.LoadEnv()
+	r := mux.NewRouter()
+	store.InitializeStore()
+	r.HandleFunc("/create/{url}", handler.CreateShortUrl)
+	r.HandleFunc("/redirect/{url}", handler.Redirect)
 }

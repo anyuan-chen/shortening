22969564e1974a4e13f44589d812f2225549cb13
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-04-16 20:21:08 -0400

    added back store loading

--- store/store_service_test.go
+++ store/store_service_test.go
@@ -3,11 +3,13 @@ package store
 import (
 	"math/rand"
 	"testing"
+	"github.com/anyuan-chen/urlshortener/server/util"
 	"github.com/stretchr/testify/assert"
 )
 var testStore = &Redis{}
 
 func init() {
+	util.LoadEnv()
 	testStore = InitializeStore()
 }
 

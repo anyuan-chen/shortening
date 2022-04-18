bbabb22fc8b32c2aa76d70e57c68c7f4292fab3d
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-04-18 18:23:29 -0400

    database functions

--- go.mod
+++ go.mod
@@ -3,21 +3,26 @@ module github.com/anyuan-chen/urlshortener/server
 go 1.18
 
 require (
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/gorilla/mux v1.8.0
+	github.com/itchyny/base58-go v0.2.0
+	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5
+)
+
+require (
 	cloud.google.com/go v0.65.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cockroachdb/cockroach-go v2.0.1+incompatible // indirect
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/itchyny/base58-go v0.2.0 // indirect
 	github.com/jessevdk/go-flags v1.5.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/lib/pq v1.10.5 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.1.0 // indirect
 	github.com/stretchr/testify v1.7.1 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
-	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5 // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
--- users/userInfo.go
+++ users/userInfo.go
@@ -1,2 +1,65 @@
 package users
 
+import (
+	"context"
+	"database/sql"
+	"log"
+
+	"github.com/cockroachdb/cockroach-go/crdb"
+)
+
+func GetUser (db *sql.DB, id string) (string, string) {
+	rows, err := db.Query("SELECT * FROM users WHERE id= $1", id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	var googleid, profileUrl string
+	var count int
+	for rows.Next(){
+		if err := rows.Scan(&googleid, &profileUrl); err != nil {
+			log.Fatal(err)
+		}
+		count++
+
+	}
+	if count == 0 {
+		log.Fatal("user doesn't exist")
+	} else if count > 1 {
+		log.Fatal("more than user with this id")
+	}
+	return id, profileUrl
+}
+
+func CreateUser (db *sql.DB, id string, profileUrl string ) error {
+	executeQuery := func (tx *sql.Tx, id string, pfpurl string) error {
+		if _, err := tx.Exec ("INSERT INTO users (name, 'profile url') VALUES ($1, $2)", id, profileUrl); err != nil {
+			return err
+		}
+		return nil
+	}
+	err := crdb.ExecuteTx(context.Background(), db, nil, func(tx *sql.Tx) error { 
+		return executeQuery(tx, id, profileUrl);
+	})
+	if err == nil {
+		return nil
+	}
+	return err
+} 
+
+func GetLinksByUser (db *sql.DB, id string) map[string]string {
+	rows, err := db.Query("SELECT * FROM links WHERE userid=$1", id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	linkMap := make(map[string]string)
+	for rows.Next() {
+		var short, long string
+		if err := rows.Scan(&short, &long); err != nil {
+			log.Fatal(err)
+		}
+		linkMap[short] = long
+	}
+	return linkMap
+}

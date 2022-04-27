029e8ee95ea284ad62fdf28a79a80d5d2a30c435
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-04-27 15:32:11 -0400

    finished getLinksById handler

--- pkg/api/shortener.go
+++ pkg/api/shortener.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/anyuan-chen/urlshortener/server/pkg/shortener"
 	"github.com/gorilla/mux"
 )
 
@@ -17,15 +18,21 @@ func (s *Service ) Redirect(w http.ResponseWriter, r *http.Request){
 	}
 	http.Redirect(w, r, original_link, http.StatusPermanentRedirect)
 }
-//CreateAuthenticated is meant as a way for logged in users to shorten a link.
-//This will then be accessible to them if they use the GetLinksForUserID endpoint
-func (s *Service ) CreateAuthenticated(w http.ResponseWriter, r *http.Request){
+//Create is meant as a way for logged in users to shorten a link. If a user id id provided,
+//it will be accessible to them if they use the GetLinksForUserID endpoint. If not, 
+//they will have to save the link on their own for future usage
+func (s *Service ) Create(w http.ResponseWriter, r *http.Request){
 	id := r.Context().Value("id")
 	original_url_params := r.URL.Query()["original_url"]
-	if len(original_url_params) != 1 {
+	var link shortener.Link
+	var err error
+	if len(original_url_params) > 1 {
 		http.Error(w, "bad query parameters", http.StatusBadRequest)
+	} else if len(original_url_params) == 0{
+		link, err = s.linkService.CreateAuthenticated(original_url_params[0], id.(string))
+	} else if len(original_url_params) == 1 {
+		link, err = s.linkService.CreateUnauthenticated(original_url_params[0])
 	}
-	link, err := s.linkService.CreateAuthenticated(original_url_params[0], id.(string))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -36,25 +43,19 @@ func (s *Service ) CreateAuthenticated(w http.ResponseWriter, r *http.Request){
 	defer r.Body.Close()
 	r.Body.Read(link_json)
 }
-//CreateUnauthenticated is a meant as a way for unauthenticated users to shorten a link.
-func (s *Service ) CreateUnauthenticated(w http.ResponseWriter, r *http.Request){
-	original_url_params := r.URL.Query()["original_url"]
-	if len(original_url_params) != 1 {
-		http.Error(w, "bad query parameters", http.StatusBadRequest)
-	}
-	link, err := s.linkService.CreateAuthenticated(original_url_params[0], "guest")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	link_json, err := json.Marshal(link)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	defer r.Body.Close()
-	r.Body.Read(link_json)
-}
+
 //GetLinksForUserID returns all links created by a specific user from the CreateAuthenticated
 //handler.
-func (s *Service ) GetLinksForUserID(){
-
+func (s *Service ) GetLinksForUserID(w http.ResponseWriter, r *http.Request){
+	session_id := r.Context().Value("session_id")
+	links, err := s.linkService.GetByUserID(session_id.(string))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	links_json, err := json.Marshal(links)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	defer r.Body.Close()
+	r.Body.Read(links_json)
 }
